4fdb6fe04a4308129ba355930637422ba365ab48
zealzel <zealzel@gmail.com>
2024-11-20 22:21:51 +0800

    change robottype name from Lino2 & Artic to lino2 & artic

--- client/src/setupProxyRemote.js
+++ client/src/setupProxyRemote.js
@@ -4,18 +4,14 @@ module.exports = function (app) {
   app.use(
     ["/api"],
     legacyCreateProxyMiddleware({
-      // target: "http://127.0.0.1:5000",
-      // target: "http://172.20.10.2:5000",
-      target: "http://10.0.1.55:5000",
+      target: "http://172.20.10.2:5000",
       changeOrigin: true,
     }),
   );
   app.use(
     ["/files", "/convert-pgm", "/save-image", "/crop-image", "/rotate"],
     legacyCreateProxyMiddleware({
-      // target: "http://127.0.0.1:5000",
-      // target: "http://172.20.10.2:5000",
-      target: "http://10.0.1.55:5000",
+      target: "http://172.20.10.2:5000",
       changeOrigin: true,
     }),
   );
