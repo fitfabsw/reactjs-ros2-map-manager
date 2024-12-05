3f0f41f3f1fcdf62e8c517e5be764e0296f71581
zealzel <zealzel@gmail.com>
2024-12-05 17:14:07 +0800

    feat: add card highlight color when some robot status is not expected

--- client/src/components/roslibjs/RobotInfo.js
+++ client/src/components/roslibjs/RobotInfo.js
@@ -4,7 +4,6 @@ import ImuData from "./ImuData";
 import Card from "react-bootstrap/Card";
 
 const RobotInfo = ({ ros, robot_namespace }) => {
-  // const [myData, setMyData] = useState(null);
   const [robotStatus, setRobotStatus] = useState(null);
   const [targetStation, setTargetStation] = useState(null);
   const [currentStation, setCurrentStation] = useState({
@@ -36,11 +35,9 @@ const RobotInfo = ({ ros, robot_namespace }) => {
       messageType: "fitrobot_interfaces/msg/Station",
     });
     targetStationTopic.subscribe((message) => {
-      const { type, name, x, y, z, w } = message;
-      setTargetStation(() => {
-        return message;
-      });
+      setTargetStation(() => message);
     });
+
     const currentStationClient = new ROSLIB.Service({
       ros: ros,
       name: `${robot_namespace}/list_current_stations`,
@@ -49,46 +46,37 @@ const RobotInfo = ({ ros, robot_namespace }) => {
     currentStationClient.callService(
       new ROSLIB.ServiceRequest(),
       function (result) {
-        console.log("SERVICE! listcurrentstation");
         if (result && Array.isArray(result.station_list)) {
-          console.log("GGG");
-          console.log(result.station_key);
-          const station_key = result.station_key;
-          const map_key = result.map_key;
-          setCurrentStation(() => {
-            return { station_key, map_key, stations: result.station_list };
-          });
-          // Station[] station_list
-          // string type
-          // string name
-          // float64 x
-          // float64 y
-          // float64 z
-          // float64 w
-          // string map_key
-          // string station_key
-          // setRobotInfo(result.robot_info_list);
-          // setRobotNamespaces(
-          //   result.robot_info_list.map((robot) => robot.robot_namespace),
-          // );
+          const { station_key, map_key } = result;
+          setCurrentStation(() => ({
+            station_key,
+            map_key,
+            stations: result.station_list,
+          }));
         } else {
-          console.log("JJ2");
-          setCurrentStation(() => {
-            return {
-              station_key: "",
-              map_key: "",
-              stations: [],
-            };
-          });
+          setCurrentStation(() => ({
+            station_key: "NA",
+            map_key: "NA",
+            stations: [],
+          }));
         }
       },
     );
   }, [ros]);
-  // }, []);
+
+  const robotStatusCardStyle =
+    robotStatus && robotStatus.status < 10
+      ? { backgroundColor: "#f8d7da", borderColor: "#f5c6cb" }
+      : {};
+
+  const currentStationCardStyle =
+    currentStation.station_key === ""
+      ? { backgroundColor: "#f8d7da", borderColor: "#f5c6cb" }
+      : {};
 
   return (
     <>
-      <Card className="mb-4 fixed-card">
+      <Card className="mb-4 fixed-card" style={robotStatusCardStyle}>
         <Card.Body>
           <Card.Title>RobotStatus</Card.Title>
           <Card.Text>
@@ -105,25 +93,22 @@ const RobotInfo = ({ ros, robot_namespace }) => {
         <Card.Body>
           <Card.Title>Target Station</Card.Title>
           {targetStation
-            ? `TargetStation: ${targetStation.name} | (${targetStation.x},{" "}
-                ${targetStation.y}, ${targetStation.z}, ${targetStation.w})`
+            ? `TargetStation: ${targetStation.name} | (${targetStation.x}, ${targetStation.y}, ${targetStation.z}, ${targetStation.w})`
             : `TargetStation: `}
         </Card.Body>
       </Card>
       <ImuData ros={ros} robot_namespace={robot_namespace} />
-      {
-        <Card className="mb-4 fixed-card">
-          <Card.Body>
-            <Card.Title>Current Stations</Card.Title>
-            <Card.Text>
-              {`station_key: ${currentStation.station_key}`}
-              <br />
-              {`map_key: ${currentStation.map_key}`}
-              <br />
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      }
+      <Card className="mb-4 fixed-card" style={currentStationCardStyle}>
+        <Card.Body>
+          <Card.Title>Current Stations</Card.Title>
+          <Card.Text>
+            {`station_key: ${currentStation.station_key}`}
+            <br />
+            {`map_key: ${currentStation.map_key}`}
+            <br />
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </>
   );
 };
