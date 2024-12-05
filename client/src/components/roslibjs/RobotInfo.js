f16f4a551de90175926de98fcb09af22f5afd141
zealzel <zealzel@gmail.com>
2024-12-05 16:42:56 +0800

    feat: add current station info to RobotInfo component

--- client/src/components/roslibjs/RobotInfo.js
+++ client/src/components/roslibjs/RobotInfo.js
@@ -7,6 +7,11 @@ const RobotInfo = ({ ros, robot_namespace }) => {
   // const [myData, setMyData] = useState(null);
   const [robotStatus, setRobotStatus] = useState(null);
   const [targetStation, setTargetStation] = useState(null);
+  const [currentStation, setCurrentStation] = useState({
+    station_key: "NA",
+    map_key: "NA",
+    stations: [],
+  });
 
   useEffect(() => {
     if (!ros) {
@@ -36,7 +41,50 @@ const RobotInfo = ({ ros, robot_namespace }) => {
         return message;
       });
     });
-  }, []);
+    const currentStationClient = new ROSLIB.Service({
+      ros: ros,
+      name: `${robot_namespace}/list_current_stations`,
+      serviceType: "fitrobot_interfaces/srv/ListCurrentStation",
+    });
+    currentStationClient.callService(
+      new ROSLIB.ServiceRequest(),
+      function (result) {
+        console.log("SERVICE! listcurrentstation");
+        if (result && Array.isArray(result.station_list)) {
+          console.log("GGG");
+          console.log(result.station_key);
+          const station_key = result.station_key;
+          const map_key = result.map_key;
+          setCurrentStation(() => {
+            return { station_key, map_key, stations: result.station_list };
+          });
+          // Station[] station_list
+          // string type
+          // string name
+          // float64 x
+          // float64 y
+          // float64 z
+          // float64 w
+          // string map_key
+          // string station_key
+          // setRobotInfo(result.robot_info_list);
+          // setRobotNamespaces(
+          //   result.robot_info_list.map((robot) => robot.robot_namespace),
+          // );
+        } else {
+          console.log("JJ2");
+          setCurrentStation(() => {
+            return {
+              station_key: "",
+              map_key: "",
+              stations: [],
+            };
+          });
+        }
+      },
+    );
+  }, [ros]);
+  // }, []);
 
   return (
     <>
@@ -63,6 +111,19 @@ const RobotInfo = ({ ros, robot_namespace }) => {
         </Card.Body>
       </Card>
       <ImuData ros={ros} robot_namespace={robot_namespace} />
+      {
+        <Card className="mb-4 fixed-card">
+          <Card.Body>
+            <Card.Title>Current Stations</Card.Title>
+            <Card.Text>
+              {`station_key: ${currentStation.station_key}`}
+              <br />
+              {`map_key: ${currentStation.map_key}`}
+              <br />
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      }
     </>
   );
 };
